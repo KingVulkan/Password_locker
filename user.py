3f8d67678420c29f32e761d3ee6d7735659f7714
KingVulkan <manowfelow@gmail.com>
2019-01-30 09:54:39 +0300

    creates a user class

---
+++ user.py
@@ -0,0 +1,11 @@
+# import unittest
+class User :
+
+    user_list = []
+    def __init__ (self, account, username, password):
+        self.account = account
+        self.username = username
+        self.password = password
+
+    contact_list = [] # Empty contact list
+ # Init method up here
