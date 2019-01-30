40c4b36d1222e0837e1519b135fa545e6c7c1e72
KingVulkan <manowfelow@gmail.com>
2019-01-30 09:35:23 +0300

    adds main  function

--- display.py
+++ display.py
@@ -21,4 +21,15 @@ def check_existing_user(account):
 def display_users():
     return User.display_users()
 #------------------------------------------------------------
-   
+def main():
+    print("\033[1;36;40m PASSWORD LOCKER App\n")
+    print("")
+    prom = input("Hello, welcome to Password Locker  whats your name. ")
+    print("  ")
+    print("hello, " + prom + ", To get you started up kindly choose one of the following .")
+    print(" ")
+    print("\033[1;34;1m  Options On how to Get Started on Password Locker\n")
+    print("")
+    # print("\033[1;35;1m cc -> To Create an Account\n")
+    # print("\033[1;35;1m Log -> To log in\n")
+    # print("\033[1;35;1m ex -> Exit\n")   
