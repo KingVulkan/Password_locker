f5f6ba9910434b4b14a5a71211e080b8f48db6dd
KingVulkan <manowfelow@gmail.com>
2019-01-30 09:59:01 +0300

    creates class methods

--- user.py
+++ user.py
@@ -16,3 +16,25 @@ class User :
         '''
 
         User.user_list.append(self)
+    def delete_user(self):
+        User.user_list.remove(self)
+    @classmethod
+    def find_by_account(cls, account):
+        for user in cls.user_list:
+            if user.account == account:
+                return user
+    @classmethod
+    def user_exist(cls, account):
+        for user in cls.user_list:
+            if user.account == account:
+                return True
+        
+        return False
+    @classmethod
+    def display_users(cls):
+        return cls.user_list
+
+    @classmethod
+    def display_user(cls):
+        for user in cls.user_list:
+            return user    
