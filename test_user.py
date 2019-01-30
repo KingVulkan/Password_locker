db5b3fcbc5e261f2355dc53bc9e25575b83bfea0
KingVulkan <manowfelow@gmail.com>
2019-01-30 09:44:48 +0300

    adds a test for saving a   user

--- test_user.py
+++ test_user.py
@@ -41,3 +41,7 @@ class TestUser(unittest.TestCase):
             tearDown method that does clean up after each test case has run.
             '''
             User.user_list = []
+
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)  
