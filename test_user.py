8faa43b37b9cf8a9d75722a9c308fb3d2ce8e9b2
KingVulkan <manowfelow@gmail.com>
2019-01-30 09:27:38 +0300

    adds a class testuser

---
+++ test_user.py
@@ -0,0 +1,14 @@
+import unittest
+import random
+from user import User # Importing the user class
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Argumentss:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    # Items up here .......
+
